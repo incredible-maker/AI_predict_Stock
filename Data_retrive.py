a822197cd9f4a471981a9486bbab6b6154b84e42
Jacky <jacky37583580@gmail.com>
2023-07-03 17:23:41 +0200

    script

--- Data_retrive.py
+++ Data_retrive.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+from datetime import datetime
+from sklearn.linear_model import LinearRegression
 
 def get_stock_info(ticker):
     stock = yf.Ticker(ticker)
@@ -9,7 +11,10 @@ def get_stock_info(ticker):
     print("Industry:", stock_info['industry'])
     print("Country:", stock_info['country'])
     print("Website:", stock_info['website'])
-    
+
+    current_year = datetime.now().year
+    previous_year = current_year - 1
+
     while True:
         print("\nPlease select an indicator to view:")
         print("1. Historical Market Data")
@@ -22,9 +27,10 @@ def get_stock_info(ticker):
         print("8. Major Holders")
         print("9. Institutional Holders")
         print("10. Earnings Dates")
+        print("11. Predict Upcoming Year Value")
         print("0. Exit")
         
-        choice = input("Enter your choice (0-10): ")
+        choice = input("Enter your choice (0-11): ")
         
         if choice == "0":
             break
@@ -52,17 +58,26 @@ def get_stock_info(ticker):
         elif choice == "5":
             income_stmt = stock.income_stmt
             print("\nIncome Statement:")
-            print(income_stmt)
+            print("Current Year:", current_year)
+            print(income_stmt.loc[current_year])
+            print("Previous Year:", previous_year)
+            print(income_stmt.loc[previous_year])
         
         elif choice == "6":
             balance_sheet = stock.balance_sheet
             print("\nBalance Sheet:")
-            print(balance_sheet)
+            print("Current Year:", current_year)
+            print(balance_sheet.loc[current_year])
+            print("Previous Year:", previous_year)
+            print(balance_sheet.loc[previous_year])
         
         elif choice == "7":
             cashflow = stock.cashflow
             print("\nCash Flow Statement:")
-            print(cashflow)
+            print("Current Year:", current_year)
+            print(cashflow.loc[current_year])
+            print("Previous Year:", previous_year)
+            print(cashflow.loc[previous_year])
         
         elif choice == "8":
             major_holders = stock.major_holders
@@ -78,6 +93,21 @@ def get_stock_info(ticker):
             earnings_dates = stock.earnings_dates
             print("\nEarnings Dates:")
             print(earnings_dates)
+        
+        elif choice == "11":
+            # Perform linear regression
+            history = stock.history(period="2y")
+            X = history[['Open']].values
+            y = history[['Open']].shift(-1).fillna(0).values
+            model = LinearRegression()
+            model.fit(X, y)
+            
+            # Predict upcoming year value
+            current_value = history.iloc[-1]['Open']
+            predicted_value = model.predict([[current_value]])[0][0]
+            print("\nPredicted Upcoming Year Value:")
+            print("Current Year Value:", current_value)
+            print("Predicted Next Year Value:", predicted_value)
         
         else:
             print("Invalid choice. Please try again.")
